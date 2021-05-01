0d0eef00750df722d3e4610148453c91626b76eb
Denis Fortict <d.fortict@vivit.fr>
2021-05-01 17:28:53 +0200

    correction intégration fontawesome dans le scss et index

---
+++ src/components/hello.jsx
@@ -0,0 +1,31 @@
+// const Hello = (props) => {
+//   return (
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      counter: 0
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked,
+      counter: this.state.counter + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className={this.state.clicked ? 'clicked': null} onClick={this.handleClick}>
+        Hello, {this.props.name} {this.state.counter}
+      </div>
+    );
+  }
+
+}
+
+export default Hello;
--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app.jsx'
 
 import '../assets/stylesheets/application.scss';
 
-// const Hello = (props) => {
-//   return (
 
-//   );
-// }
-
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      clicked: false,
-      counter: 0
-    };
-  }
-
-  handleClick = () => {
-    this.setState({
-      clicked: !this.state.clicked,
-      counter: this.state.counter + 1
-    })
-  }
-
-  render() {
-    return (
-      <div className={this.state.clicked ? 'clicked': null} onClick={this.handleClick}>
-        Hello, {this.props.name} {this.state.counter}
-      </div>
-    );
-  }
-
-}
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="Denis" />, root);
+  ReactDOM.render(
+    <App />, root);
 }
