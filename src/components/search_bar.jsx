3011fec28ac6866ec54a16b82d5c3167d4578b8b
Denis Fortict <d.fortict@vivit.fr>
2021-05-02 17:07:12 +0200

    controlled components value on search_bar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -3,16 +3,25 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            term: ''
+         }
     }
 
     handleUpdate = (event) => {
+        this.setState({
+            term: event.target.value
+        })
         this.props.searchFunction(event.target.value);
     }
 
     render() { 
         return (
-            <input type="text" className="form-control form-search" onChange={this.handleUpdate}/>
+            <input
+                value={this.state.term}
+                type="text"
+                className="form-control form-search"
+                onChange={this.handleUpdate} />
         );
     }
 }
