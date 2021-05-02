a75bbe61c234e18770a68eaf79fcf86f3a2fcbba
Denis Fortict <d.fortict@vivit.fr>
2021-05-02 23:35:45 +0200

    destructuring gif-list

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Gif from './gif';
 
-const GifList = (props) => {
+const GifList = ({gifs, selectGif}) => {
     return (
         <div className="gif-list">
-            {props.gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGif={props.selectGif}/>)}
+            {gifs.map(({id}) => <Gif id={id} key={id} selectGif={selectGif}/>)}
         </div>
     );
 }
